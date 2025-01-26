84d219412f469afd8c1c261c074c181dec0340b8
cronos <bamlakdesalgn@gmail.com>
2025-01-26 09:36:22 +0300

    another try

--- registration.php
+++ registration.php
@@ -148,7 +148,7 @@ if (isset($_POST['form1'])) {
                                         $token,
                                         $cust_datetime,
                                         $cust_timestamp,
-                                        0
+                                        1
                                     ));
 
         // Send email for confirmation of the account
@@ -179,7 +179,7 @@ if (isset($_POST['form1'])) {
         // unset($_POST['cust_state']);
         // unset($_POST['cust_zip']);
 
-        // $success_message = LANG_VALUE_152;
+        $success_message = LANG_VALUE_152;
     }
 }
 ?>
--- verify.php
+++ verify.php
@@ -1,8 +1,8 @@
 <?php require_once('header.php'); ?>
 
 <?php
-if ( (!isset($_REQUEST['email'])) || (isset($_REQUEST['token'])) )
-{
+// if ( (!isset($_REQUEST['email'])) || (isset($_REQUEST['token'])) )
+// {
     $var = 1;
 
     // check if the token is correct and match with database.
@@ -24,7 +24,7 @@ if ( (!isset($_REQUEST['email'])) || (isset($_REQUEST['token'])) )
 
         $success_message = '<p style="color:green;">Your email is verified successfully. You can now login to our website.</p><p><a href="'.BASE_URL.'login.php" style="color:#167ac6;font-weight:bold;">Click here to login</a></p>';     
     }
-}
+// }
 ?>
 
 <div class="page-banner" style="background-color:#444;">
