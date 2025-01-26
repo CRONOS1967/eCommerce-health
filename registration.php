7754aa6a3e9a203c0e413e3b715e2d2430c5b36b
cronos <bamlakdesalgn@gmail.com>
2025-01-26 10:00:07 +0300

    updated query

--- registration.php
+++ registration.php
@@ -78,78 +78,119 @@ if (isset($_POST['form1'])) {
             $error_message .= LANG_VALUE_139."<br>";
         }
     }
-
-    if($valid == 1) {
-
+    if ($valid == 1) {
+        // Generate token and timestamp
         $token = md5(time());
         $cust_datetime = date('Y-m-d h:i:s');
         $cust_timestamp = time();
-
-        // saving into the database
-        $statement = $pdo->prepare("INSERT INTO tbl_customer (
-                                        cust_name,
-                                        cust_cname,
-                                        cust_email,
-                                        cust_phone,
-                                        cust_country,
-                                        cust_address,
-                                        cust_city,
-                                        cust_state,
-                                        cust_zip,
-                                        cust_b_name,
-                                        cust_b_cname,
-                                        cust_b_phone,
-                                        cust_b_country,
-                                        cust_b_address,
-                                        cust_b_city,
-                                        cust_b_state,
-                                        cust_b_zip,
-                                        cust_s_name,
-                                        cust_s_cname,
-                                        cust_s_phone,
-                                        
-                                        cust_s_address,
-                                        cust_s_city,
-                                        cust_s_state,cust_s_country,
-                                        cust_s_zip,
-                                        cust_password,
-                                        cust_token,
-                                        cust_datetime,
-                                        cust_timestamp,
-                                        cust_status
-                                    ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-        $statement->execute(array(
-                                        strip_tags($_POST['cust_name']),
-                                        strip_tags($_POST['cust_cname']),
-                                        strip_tags($_POST['cust_email']),
-                                        strip_tags($_POST['cust_phone']),
-                                        strip_tags($_POST['cust_country']),
-                                        strip_tags($_POST['cust_address']),
-                                        strip_tags($_POST['cust_city']),
-                                        strip_tags($_POST['cust_state']),
-                                        strip_tags($_POST['cust_zip']),
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        '',
-                                        md5($_POST['cust_password']),
-                                        $token,
-                                        $cust_datetime,
-                                        $cust_timestamp,
-                                        1
-                                    ));
+    
+        // Hash the password
+        $hashed_password = password_hash($_POST['cust_password'], PASSWORD_BCRYPT);
+    
+        // Prepare the SQL statement
+        $statement = $pdo->prepare("
+            INSERT INTO tbl_customer (
+                cust_name,
+                cust_cname,
+                cust_email,
+                cust_phone,
+                cust_country,
+                cust_address,
+                cust_city,
+                cust_state,
+                cust_zip,
+                cust_b_name,
+                cust_b_cname,
+                cust_b_phone,
+                cust_b_country,
+                cust_b_address,
+                cust_b_city,
+                cust_b_state,
+                cust_b_zip,
+                cust_s_name,
+                cust_s_cname,
+                cust_s_phone,
+                cust_s_country,
+                cust_s_address,
+                cust_s_city,
+                cust_s_state,
+                cust_s_zip,
+                cust_password,
+                cust_token,
+                cust_datetime,
+                cust_timestamp,
+                cust_status
+            ) VALUES (
+                :cust_name,
+                :cust_cname,
+                :cust_email,
+                :cust_phone,
+                :cust_country,
+                :cust_address,
+                :cust_city,
+                :cust_state,
+                :cust_zip,
+                :cust_b_name,
+                :cust_b_cname,
+                :cust_b_phone,
+                :cust_b_country,
+                :cust_b_address,
+                :cust_b_city,
+                :cust_b_state,
+                :cust_b_zip,
+                :cust_s_name,
+                :cust_s_cname,
+                :cust_s_phone,
+                :cust_s_country,
+                :cust_s_address,
+                :cust_s_city,
+                :cust_s_state,
+                :cust_s_zip,
+                :cust_password,
+                :cust_token,
+                :cust_datetime,
+                :cust_timestamp,
+                :cust_status
+            )
+        ");
+    
+        // Execute the statement with bound parameters
+        $statement->execute([
+            ':cust_name' => $_POST['cust_name'],
+            ':cust_cname' => $_POST['cust_cname'] ?? '', // Use empty string if not provided
+            ':cust_email' => $_POST['cust_email'],
+            ':cust_phone' => $_POST['cust_phone'],
+            ':cust_country' => $_POST['cust_country'],
+            ':cust_address' => $_POST['cust_address'],
+            ':cust_city' => $_POST['cust_city'],
+            ':cust_state' => $_POST['cust_state'],
+            ':cust_zip' => $_POST['cust_zip'],
+            ':cust_b_name' => '', // Default empty values for billing fields
+            ':cust_b_cname' => '',
+            ':cust_b_phone' => '',
+            ':cust_b_country' => 0,
+            ':cust_b_address' => '',
+            ':cust_b_city' => '',
+            ':cust_b_state' => '',
+            ':cust_b_zip' => '',
+            ':cust_s_name' => '', // Default empty values for shipping fields
+            ':cust_s_cname' => '',
+            ':cust_s_phone' => '',
+            ':cust_s_country' => 0,
+            ':cust_s_address' => '',
+            ':cust_s_city' => '',
+            ':cust_s_state' => '',
+            ':cust_s_zip' => '',
+            ':cust_password' => $hashed_password, // Use the hashed password
+            ':cust_token' => $token,
+            ':cust_datetime' => $cust_datetime,
+            ':cust_timestamp' => $cust_timestamp,
+            ':cust_status' => 1 // Default status (active)
+        ]);
+    
+        // Success message
+        $success_message = "Registration successful!";
+    
                                     if ($statement) {
                                         echo '<meta http-equiv="refresh" content="2;url=login.php">';
                                         echo '<p>Registration successful. Redirecting to login page...</p>';
